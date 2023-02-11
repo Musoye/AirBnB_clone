e9abba3a22c66e56afc0820d174259b9b9ef2fbc
stoicism-sympathei <mogesanonymous@gmail.com>
2023-02-11 06:15:27 +0300

    console

--- console.py
+++ console.py
@@ -35,7 +35,7 @@ class HBNBCommand(cmd.Cmd):
 
 
 """
-Hi, "Mustapha" I am just trying _destroy_ task below you can write the other staff here!
+Hi, "Mustapha" I am just trying _destroy, update, count_ task below you can write the other staff here!
 """
 
 
@@ -76,3 +76,60 @@ Hi, "Mustapha" I am just trying _destroy_ task below you can write the other sta
                    if type(v) is eval(args[0])])
         else:
             print(self.errors["wrongClass"])
+
+                def do_update(self, arg):
+        """
+        Updates an instance by adding or updating attribute.
+            usage: update <class_name> <id> <attribute_name> <attribute_value>
+        """
+        args = shlex.split(arg)
+        models.storage.reload()
+        if len(args) < 1:
+            print(self.errors["missingClass"])
+        elif args[0] in self.classes:
+            if len(args) < 2:
+                print(self.errors["missingID"])
+            else:
+                key = args[0] + '.' + args[1]
+                if key in models.storage.all().keys():
+                    if len(args) < 3:
+                        print(self.errors["missingAttr"])
+                    else:
+                        if len(args) < 4:
+                            print(self.errors["missingValue"])
+                        else:
+                            obj = models.storage.all()[key]
+                            try:
+                                attr_type = type(getattr(obj, args[2]))
+                                args[3] = attr_type(args[3])
+                            except:
+                                try:
+                                    args[3] = int(args[3])
+                                except:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        pass
+
+                            setattr(obj, args[2], args[3])
+                            obj.save()
+                else:
+                    print(self.errors["wrongID"])
+        else:
+            print(self.errors["wrongClass"])
+
+    def count(self, arg):
+        """
+        Prints the number of instances of a class.
+            usage: count <class_name>
+        """
+        args = shlex.split(arg)
+        models.storage.reload()
+        if len(args) < 1:
+            print(self.errors["missingClass"])
+        elif args[0] in self.classes:
+            instances = str(models.storage.all().keys())
+            print(instances.count(args[0]))
+        else:
+            print(self.errors["wrongClass"])
+
